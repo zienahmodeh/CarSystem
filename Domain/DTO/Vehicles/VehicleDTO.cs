cca37ba90a2ac98553f9373767508152a9332236
Zeina <Zeina@e-incube.com>
2026-03-09 21:13:55 +0300

    add VehicleDTO and ICarService interface

---
+++ Domain/DTO/Vehicles/VehicleDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace DTO
+{
+    public class NhtsaResponse<T>
+    {
+        public int Count { get; set; }
+        public string Message { get; set; }
+        public List<T> Results { get; set; }
+    }
+
+    public class MakeDTO
+    {
+        public int MakeID { get; set; }
+        public string MakeName { get; set; }
+    }
+
+    public class ModelDTO
+    {
+        public int MakeID { get; set; }
+        public string MakeName { get; set; }
+        public int ModelID { get; set; }
+        public string ModelName { get; set; }
+    }
+}
---
+++ Domain/Interfaces/ICarService.cs
@@ -0,0 +1,12 @@
+﻿using DTO;
+using SharedKernel;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces;
+
+public interface ICarService
+{
+    Task<ServiceOperationResult<List<MakeDTO>>> GetAllMakes();
+    Task<ServiceOperationResult<List<ModelDTO>>> GetModelsByMakeId(int makeId);
+}
